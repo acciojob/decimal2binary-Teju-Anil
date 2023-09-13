95c88b1408fe64f2df4da34acb9383475fcd7615
Teju-Anil <127083204+Teju-Anil@users.noreply.github.com>
2023-09-13 10:16:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-function decimalToBinary(num) {
-  //Write you code here
-  
-}
+def decimal_to_binary(decimal_num):
+    binary_num = bin(decimal_num)[2:]
+    return binary_num
 
-window.decimalToBinary = decimalToBinary;
+# Example Usage
+decimal_input = int(input("Enter a decimal number: "))
+binary_output = decimal_to_binary(decimal_input)
+print(f"The binary representation is: {binary_output}")
