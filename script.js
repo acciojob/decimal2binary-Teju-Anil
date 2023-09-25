8c42358e4ad3ee97c535e5b083bc63997d3776be
Teju-Anil <127083204+Teju-Anil@users.noreply.github.com>
2023-09-25 19:43:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,23 @@
-
-// JavaScript program to convert binary to decimal
-
-// Function to convert binary to decimal
-function binaryToDecimal(n)
+function decToBinary(n)
 {
-	let num = n;
-	let dec_value = 0;
-
-	// Initializing base value to 1, i.e 2^0
-	let base = 1;
-
-	let temp = num;
-	while (temp) {
-		let last_digit = temp % 10;
-		temp = Math.floor(temp / 10);
-
-		dec_value += last_digit * base;
-
-		base = base * 2;
-	}
-
-	return dec_value;
+    // array to store binary number
+    let binaryNum = new Array(32);
+ 
+    // counter for binary array
+    let i = 0;
+    while (n > 0) {
+ 
+        // storing remainder in binary array
+        binaryNum[i] = n % 2;
+        n = Math.floor(n / 2);
+        i++;
+    }
+ 
+    // printing binary array in reverse order
+    for (let j = i - 1; j >= 0; j--)
+        document.write(binaryNum[j]);
 }
-
+ 
 // Driver program to test above function
-	let num = 10101001;
-
-	document.write(binaryToDecimal(num) + "<br>");
-
-
-// This code is contributed by Surbhi Tyagi
+    let n = 7;
+    decToBinary(n);
